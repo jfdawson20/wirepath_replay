33673e91ebca6d588925791a9e1cd74d3869817f
jack <john.f.dawson20@gmail.com>
2025-12-24 08:23:11 -0500

    modifying example init

--- tools/example_init.sh
+++ tools/example_init.sh
@@ -4,8 +4,6 @@
 modprobe vfio-pci 
 
 #bind PCI devices to vfio-pci driver
-dpdk-devbind.py -b vfio-pci 0000:01:00.0
-dpdk-devbind.py -b vfio-pci 0000:01:00.1
 
 #reserve 1G hugepage 
 ./tools/make_hugepagefs.sh 8
